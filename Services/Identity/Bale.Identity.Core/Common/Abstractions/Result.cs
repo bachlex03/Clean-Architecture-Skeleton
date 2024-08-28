bff8bb18ffdfd0f888e1fe26b5e304271c088684
bachlex1608 <lxbachit03.working@gmail.com>
2024-08-28 09:32:04 +0700

    patterns(Result): implement result pattern #1

---
+++ Services/Identity/Bale.Identity.Core/Common/Abstractions/Result.cs
@@ -0,0 +1,26 @@
+﻿using Bale.Identity.Core.Common.Errors;
+
+namespace Bale.Identity.Core.Common.Abstractions;
+
+public class Result
+{
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public Error Error { get; }
+
+    public Result(bool isSuccess, Error error)
+    {
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+}
+
+public class Result<TResponse> : Result
+{
+    public TResponse Respone { get; }
+
+    public Result(bool isSuccess, TResponse response, Error error) : base(isSuccess, error)
+    {
+        Respone = response;
+    }
+}
